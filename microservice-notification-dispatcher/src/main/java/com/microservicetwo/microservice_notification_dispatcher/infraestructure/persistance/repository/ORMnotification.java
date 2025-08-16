83fcc4b31751723132d7ab619edc8fd9dd19cd10
Agudelo3075 <sebasagudelo1012@gmail.com>
2025-08-15 23:57:25 -0500

    Agregando ORM de notification y users

--- microservice-notification-dispatcher/src/main/java/com/microservicetwo/microservice_notification_dispatcher/infraestructure/persistance/repository/ORMnotification.java
+++ microservice-notification-dispatcher/src/main/java/com/microservicetwo/microservice_notification_dispatcher/infraestructure/persistance/repository/ORMnotification.java
@@ -1,8 +1,10 @@
 package com.microservicetwo.microservice_notification_dispatcher.infraestructure.persistance.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.microservicetwo.microservice_notification_dispatcher.infraestructure.persistance.entity.NotificationEntity;
 
 @Repository
-public interface ORMnotification {
+public interface ORMnotification extends JpaRepository<NotificationEntity, String> {
     
 } 

7c570982e297ea10077fd2d06d0382858a34728d
payan3521 <pablobedoya3521@gmail.com>
2025-08-18 14:59:44 -0500

    paso a dockerizar

--- microservice-notification-dispatcher/src/main/java/com/microservicetwo/microservice_notification_dispatcher/infraestructure/persistance/adapter/AdapterUser.java
+++ microservice-notification-dispatcher/src/main/java/com/microservicetwo/microservice_notification_dispatcher/infraestructure/persistance/adapter/AdapterUser.java
@@ -1,9 +1,7 @@
 package com.microservicetwo.microservice_notification_dispatcher.infraestructure.persistance.adapter;
 
 import java.util.Optional;
-
 import org.springframework.stereotype.Component;
-
 import com.microservicetwo.microservice_notification_dispatcher.domain.model.User;
 import com.microservicetwo.microservice_notification_dispatcher.domain.port.out.IPortUser;
 import com.microservicetwo.microservice_notification_dispatcher.infraestructure.persistance.entity.UserEntity;
@@ -19,12 +17,12 @@ public class AdapterUser implements IPortUser {
     private final UserMapper userMapper;
 
     @Override
-    public boolean existsByEmail(String email) {
+    public boolean existsByEmail(String email) { //no se estan usando
         return ormUser.existsByEmail(email);
     }
 
     @Override
-    public boolean existsByPhoneNumber(String phoneNumber) {
+    public boolean existsByPhoneNumber(String phoneNumber) {   //no se estan usando
         return ormUser.existsByPhoneNumber(phoneNumber);
     }
 
